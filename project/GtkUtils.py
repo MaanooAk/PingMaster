3942246f8a0bb31abfb9764412af7cfaa9f8bd8a
MaanooAk <akritasak@gmail.com>
2018-03-30 16:16:01 +0300

    Simple gtk util functions

---
+++ project/GtkUtils.py
@@ -0,0 +1,32 @@
+
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+def button(name, click, data=None):
+    b = Gtk.Button(name)
+    b.set_hexpand(True)
+    if data is not None:
+        b.connect("clicked", click, data)
+    else:
+        b.connect("clicked", click)
+    return b
+
+
+def image_button(stock_image, click, data=None):
+    b = Gtk.Button()
+    b.add(Gtk.Image(stock=stock_image))
+    if data is not None:
+        b.connect("clicked", click, data)
+    else:
+        b.connect("clicked", click)
+    return b
+
+
+def wrap_into_scrolled(o):
+    sw = Gtk.ScrolledWindow()
+    sw.set_hexpand(True)
+    sw.set_vexpand(True)
+    sw.add(o)
+    return sw
