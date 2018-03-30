d39cca75af1856ea241c0c9b068b8d3d309d688b
MaanooAk <akritasak@gmail.com>
2018-03-30 16:15:04 +0300

    Pinger interface and os based impl

---
+++ project/Pinger.py
@@ -0,0 +1,19 @@
+
+from subprocess import call, DEVNULL
+
+
+class Pinger:
+    def ping(self, hostname: str) -> int:
+        """Returns an int representing the ping result
+
+        Return codes:
+        - 0: success
+        - 1: not reply
+        - 2: error
+        """
+        return 2
+
+
+class OsPinger(Pinger):
+    def ping(self, hostname: str) -> int:
+        return call(["ping", "-c", "1", "-W", "1", hostname], stdout=DEVNULL, stderr=DEVNULL)
